2160a52d28100a72ad99a1fcc21eb01e63e070fc
tetrixomaldi <manonimamulia8@gmail.com>
2023-11-23 21:55:24 +0000

    Updating grew

---
+++ 23112023_215524_grew_d6z0i.sh
@@ -0,0 +1,1 @@
+The first box of Crayola that was ever sold had the same eight colours that are sold in the box today consisting of red, blue, yellow, green, violet, orange, black and brown. The box was sold for a nickel in 1903
